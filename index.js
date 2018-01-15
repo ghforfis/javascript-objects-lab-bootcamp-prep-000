da6c8cfe2c986d535f6b21eec3a2d5d071354ccb
Frankie G <ftgalasso@gmail.com>
2018-01-15 20:49:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,19 +14,9 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-      var obj = { prop: 1 }
-
-      expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
-        prop: 1,
-        prop2: 2
-      })
-
-      expect(obj).toMatch({
-        prop: 1,
-        prop2: 2
-      })
-    })
-  })
+  var obj = { prop: 1 }
+  
+  return obj
 }
 
 
