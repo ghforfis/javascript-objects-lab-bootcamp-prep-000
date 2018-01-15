d4324d309feb260abcaa42a6e61c204d57e782c9
Frankie G <ftgalasso@gmail.com>
2018-01-15 20:47:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,20 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return obj
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var obj = { prop: 1};
-  obj = {
-    [key]: value
-  }
-  return obj
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+      var obj = { prop: 1 }
+
+      expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+
+      expect(obj).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+  })
 }
 
 
