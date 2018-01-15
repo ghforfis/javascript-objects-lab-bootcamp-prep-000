b2bf9579f08f08b090b13345b901265201e62f08
Frankie G <ftgalasso@gmail.com>
2018-01-15 19:06:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return recipes;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object.[key] = value;
  
   return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object[key];
+  delete object.key;
   return object;
 }
 
