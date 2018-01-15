25977c0517b25bdeb2d1433ba4c68306b0fa99da
Frankie G <ftgalasso@gmail.com>
2018-01-15 18:56:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return obj
 }
 function deleteFromObjectByKey(object, key) {
-  
+  delete object [key];
+  return object
+}
+
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object
 }
