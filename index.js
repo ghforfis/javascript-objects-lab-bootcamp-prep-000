10ce7648f59db932c01e6b3013ca21fcb3fe3590
Frankie G <ftgalasso@gmail.com>
2018-01-15 19:38:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   var recipes = {
-    prop: 1
+    prop: 1,
+    prop2: 2,
   }
   
   return recipes;
