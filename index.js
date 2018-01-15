7283702fd24f886a4ca551c10946c409ef9f2192
Frankie G <ftgalasso@gmail.com>
2018-01-15 21:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,35 +4,3 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-function updateObjectWithKeyAndValue(obj, key, value) {
-  var obj = { prop: 1 };
-  obj = {
-        prop: 1,
-        prop2: 2
-      };
-  return obj
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var obj = {prop: 1};
-  var object = Object.assign(obj, { prop2: 2 } );
-  return object
-}
-
-function destructivelyDeleteFromObjectByKey(obj, key){
-  delete obj[key];
-  return obj;
-}
-
-
-
-function deleteFromObjectByKey(obj, key){
- 
-var newObj = Object.assign({}, recipes)
- 
-newObj 
- 
-delete newObj.key
- 
-newObj
-}
