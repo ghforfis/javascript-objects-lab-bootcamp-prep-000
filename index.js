6d3adb86c702fcb658d9939f00a7d1f70c2f1f69
Frankie G <ftgalasso@gmail.com>
2018-01-15 22:14:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return obj
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
       
-  object[key] = value;
+  obj[key] = value;
   
 }
 
